96adbea6443eaa4b37c08eb9e26bb43ea123d8ef
Sabuj Mondal <hallosabuj@gmail.com>
2021-07-01 00:16:30 +0530

    Functional option code modified

--- functional-option/functional-option1.go
+++ functional-option/functional-option1.go
@@ -10,42 +10,15 @@ type Car struct {
 	colour string
 }
 
-type Option func(*Car)
-
-func Make(v string) Option {
-	return func(c *Car) {
-		c.make = v
-	}
-}
-func Model(v string) Option {
-	return func(c *Car) {
-		c.model = v
-	}
-}
-func Colour(v string) Option {
-	return func(c *Car) {
-		c.colour = v
-	}
-}
-
-func NewCar(opts ...Option) *Car {
-	c := &Car{
+func NewCar() *Car {
+	return &Car{
 		make:   "BMW",
 		model:  "3.18",
 		colour: "Silver",
 	}
-
-	for _, opt := range opts {
-		opt(c)
-	}
-
-	return c
 }
 
 func main() {
-	car1 := NewCar()
-	fmt.Printf("%+v\n", car1)
-
-	car2 := NewCar(Make("Mercedes"), Model("SEL 500"))
-	fmt.Printf("%+v\n", car2)
+	car := NewCar()
+	fmt.Printf("%+v\n", car)
 }
